42b82e0d69487e4f94c9d5b31ee207efaa035d1b
Marwan Ahmed <marwan_ahmed65@yahoo.com>
2019-11-20 16:55:33 +0200

    front edits

--- src/app/friends/friend-details/friend-details.component.ts
+++ src/app/friends/friend-details/friend-details.component.ts
@@ -1,6 +1,7 @@
 import { FriendService } from 'src/app/services/friend.service';
 import { Component, Input, ViewChild, ElementRef } from '@angular/core';
 import { Friend } from 'src/app/friend.model';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-friend-details',
@@ -21,3 +22,15 @@ export class FriendDetailsComponent {
   }
 
 }
+
+$(Document).ready(function () {
+  $('.close-icon').on('click', function () {
+    $('.chat-page').css({display: 'none', visibility : 'hidden'});
+    $('.chat-page-button').css({display : 'block', visibility : 'visible'});
+  })
+
+  $('.chat-page-button').on('click', function () {
+    $(this).css({display: 'none', visibility : 'hidden'});
+    $('.chat-page').css({display : 'block', visibility : 'visible'});
+  })
+})
